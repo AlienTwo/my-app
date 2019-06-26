4982cb5fb3ac7f120ae083c4f5a8434e211827da
wang yadong <994480559@qq.com>
2019-06-26 10:01:35 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+
+	const [count, setCount] = useState(0);
+	const [age, setAge] = useState(22);
+
+	useEffect(() => {
+		document.title = `You click ${count} times`
+	})
+	
+	return (
+		<div className="App">
+		<p>You clicked {count} times</p>
+		<p>my age is {age}</p>
+		<button onClick={() => {
+			setCount(count + 1);
+			setAge(age + 1);
+		}}>
+			Click me
+		</button>
+		</div>
+	);
+}
+
+export default App;
