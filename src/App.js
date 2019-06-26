9448201059368fd927b7419dd3068791a02f9e72
wang yadong <994480559@qq.com>
2019-06-26 16:09:30 +0800

    test1 commit-4

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 	return (
 		<div className="App">
 		<p>You clicked {count} times</p>
-		<p>my age is {age}</p>
+		<p>dong's age is {age}</p>
 		<button onClick={() => {
 			setCount(count + 1);
 			setAge(age + 1);
