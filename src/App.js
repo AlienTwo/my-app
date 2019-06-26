246de7c7f26acc2cb4761acd525970d09705810f
wang yadong <994480559@qq.com>
2019-06-26 13:58:00 +0800

    YYY

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 	const [age, setAge] = useState(22);
 
 	useEffect(() => {
-		document.title = `You click ${count} times develop test`
+		document.title = `YYYYou click ${count} times develop test`
 	})
 	
 	return (
