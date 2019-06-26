b5ad43e0ce071278b6cf101424abdc39b9a8113a
wang yadong <994480559@qq.com>
2019-06-26 14:35:26 +0800

    rebase test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 	const [age, setAge] = useState(22);
 
 	useEffect(() => {
-		document.title = `YYYYou click ${count} times rebase test`
+		document.title = `YYYYou click ${count} times rebase test2`
 	})
 	
 	return (
