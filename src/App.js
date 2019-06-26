e3d2d34c1ff452ee3671929cfeb5ed900b8db47f
wang yadong <994480559@qq.com>
2019-06-26 14:57:10 +0800

    change word

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 	const [age, setAge] = useState(22);
 
 	useEffect(() => {
-		document.title = `YYYYou click ${count} times still develop test`
+		document.title = `YYYYou click ${count} times still develop test v1.1`
 	})
 	
 	return (
